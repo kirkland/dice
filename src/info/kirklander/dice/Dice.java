452cb2a8dabf3fdfcf5a586753d1700c2cbe535d
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-01-30 20:51:05 -0500

    show one die image

--- src/info/kirklander/dice/Dice.java
+++ src/info/kirklander/dice/Dice.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Dice extends Activity {
     /** Called when the activity is first created. */
@@ -16,6 +17,9 @@ public class Dice extends Activity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 setContentView(R.layout.dice);
+                
+                ImageView iv = (ImageView) findViewById(R.id.ImageView01);
+                iv.setImageResource(R.drawable.dice1);
             }
         });
     }
