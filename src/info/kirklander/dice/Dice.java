e3cc3ed1972f1e1666c5efac8aa07e2dc757c708
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-01-30 21:14:03 -0500

    shows one random die

--- src/info/kirklander/dice/Dice.java
+++ src/info/kirklander/dice/Dice.java
@@ -1,6 +1,9 @@
 package info.kirklander.dice;
 
+import java.util.Random;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,11 +19,17 @@ public class Dice extends Activity {
         final Button button = (Button) findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+            	Random generator = new Random();
+            	String diceIndex = "" + (generator.nextInt(6) + 1);
+            	String resource = "dice" + diceIndex;
+            	
                 setContentView(R.layout.dice);
                 
                 ImageView iv = (ImageView) findViewById(R.id.ImageView01);
-                iv.setImageResource(R.drawable.dice1);
+                iv.setImageResource(getResources().getIdentifier(resource, "drawable", getPackageName()));
+                
             }
         });
     }
+
 }
