e2a1f3db11190ecc63d8e727f0af9398ee9f4cfa
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-01-30 20:14:50 -0500

    click button, go to blank screen

--- src/info/kirklander/dice/Dice.java
+++ src/info/kirklander/dice/Dice.java
@@ -2,6 +2,8 @@ package info.kirklander.dice;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Dice extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,12 @@ public class Dice extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.dice);
+            }
+        });
     }
 }
