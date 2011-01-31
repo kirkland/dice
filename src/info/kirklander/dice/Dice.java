68a970c3ef9343ca9930dce1d737adab6ae5854c
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-01-30 19:41:26 -0500

    initial commit

---
+++ src/info/kirklander/dice/Dice.java
@@ -0,0 +1,13 @@
+package info.kirklander.dice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Dice extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
